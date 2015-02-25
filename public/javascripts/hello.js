8f0cc35c6785119ac40cca63bb3b3e2286c3d0a7
kiquenet85 <kiquenet85@gmail.com>
2015-02-24 22:02:50 -0500

    1. Adding Template responsive design page, from http://www.lingulo.com/tutorials/css/how-to-build-a-html5-website-from-scratch 2. Adding mock images from s2s philosophy. 3. Modified login into the first page using (index,main) Views.

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,3 +1,26 @@
 if (window.console) {
   console.log("Welcome to your Play application's JavaScript!");
 }
+
+(function ($, window, document, undefined)
+{
+	'use strict';
+
+	$(function ()
+	{
+	    $("#mobileMenu").hide();
+	    $(".toggleMobile").click(function()
+	    {
+	    	$(this).toggleClass("active");
+	        $("#mobileMenu").slideToggle(500);
+	    });
+	});
+	$(window).on("resize", function()
+	{
+		if($(this).width() > 500)
+		{
+			$("#mobileMenu").hide();
+			$(".toggleMobile").removeClass("active");
+		}
+	});
+})(jQuery, window, document);
